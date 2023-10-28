98546aad933b759adea1387818cf54e3586ca43f
thisthebest <54426097+thisthebest@users.noreply.github.com>
2023-10-28 14:21:21 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,13 +145,13 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          雀特鸡皮提
+          AI助手.
         </div>
         <div className={styles["sidebar-sub-title"]}>
           由 阿豹 提供key服务.
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          仅支持3.5/3.5-16l和4.0/4.0-32k.
+          仅支持3.5/3.5-16k和4.0/4.0-32k.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
